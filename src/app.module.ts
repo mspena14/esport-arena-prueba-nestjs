2bb074c0398533f4dedfd0ba08ed8e89e496c8b8
mspena14 <marlono1naranjo@gmail.com>
2024-10-07 16:33:13 -0400

    feat: database connection implemented in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    })
+    ,TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT , 10) || 27447,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: true,
+        ca: process.env.DB_SSL_CA,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
